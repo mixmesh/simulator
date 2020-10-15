ce15301b6fc1945bbb5813010099d8797223f804
Joakim G <joagre@gmail.com>
2020-10-15 13:42:55 +0200

    Added SSL support to POP3/SMTP including certificate handling and config in obscrete.conf. Added a PIN to obscrete.conf

--- src/simulator_mkconfig.erl
+++ src/simulator_mkconfig.erl
@@ -5,12 +5,13 @@
 
 -spec start([string()]) -> no_return().
 
-start([DataSet]) ->
+start([SourceCertFilename, DataSet]) ->
     ObscreteDir = <<"/tmp/obscrete">>,
     PkiDataDir = filename:join([ObscreteDir, <<"pki">>, <<"data">>]),
     ok = mkconfig:ensure_libs([PkiDataDir], true),
     PlayersDir = filename:join([ObscreteDir, "players"]),
-    create_players(PlayersDir, get_location_index(DataSet)),
+    create_players(SourceCertFilename, PlayersDir,
+                   get_location_index(DataSet)),
     mkconfig:return(0).
 
 get_location_index("square") ->
@@ -24,18 +25,29 @@ get_location_index("roma") ->
 get_location_index("it") ->
     it_vr2marketbaiaotrial:get_location_index().
 
-create_players(_PlayersDir, []) ->
+create_players(_SourceCertFilename, _PlayersDir, []) ->
     ok;
-create_players(PlayersDir, [{Name, _}|Rest]) ->
+create_players(SourceCertFilename, PlayersDir, [{Name, _}|Rest]) ->
     PlayerDir = filename:join([PlayersDir, Name, <<"player">>]),
     PlayerTempDir = filename:join([PlayerDir, "temp"]),
     PlayerBufferDir = filename:join([PlayerDir, "buffer"]),
     PlayerPkiDataDir = filename:join([PlayerDir, "pki", "data"]),
     PlayerMaildropSpoolerDir =
         filename:join([PlayerDir, "maildrop", "spooler"]),
+    PlayerSSLDir = filename:join([PlayerDir, "ssl"]),
     mkconfig:ensure_libs(
       [PlayerTempDir,
        PlayerBufferDir,
        PlayerPkiDataDir,
-       PlayerMaildropSpoolerDir], true),
-    create_players(PlayersDir, Rest).
+       PlayerMaildropSpoolerDir,
+       PlayerSSLDir], true),
+    TargetCertFilename = filename:join([PlayerDir, "ssl", "cert.pem"]),
+    io:format("Copies ~s to ~s\n", [SourceCertFilename, TargetCertFilename]),
+    case file:copy(SourceCertFilename, TargetCertFilename) of
+        {ok, _} ->
+            create_players(SourceCertFilename, PlayersDir, Rest);
+        {error, Reason} ->
+            io:format(standard_error, "~s: ~s\n",
+                      [SourceCertFilename, file:format_error(Reason)]),
+            mkconfig:return(100)
+    end.
